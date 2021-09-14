45f02906e00293d0ad18dcc75dfa73dc0f47748e
Michelangelo <mhlmrt@gmail.com>
2021-09-14 23:31:38 +0200

    manageCSV2

--- src/main/java/ManageCSV.java
+++ src/main/java/ManageCSV.java
@@ -32,6 +32,10 @@ public class ManageCSV {
         countSeverity[3]=0;
     }
 
+
+
+
+
     public ArrayList<String[]> getTuples(Date dateStart, int granularity) throws IOException {
         String header = "ID,Severity,Start_Time,End_Time,Start_Lat,Start_Lng,End_Lat,End_Lng,Distance(mi),Description,Number,Street,Side,City,County,State,Zipcode,Country,Timezone,Airport_Code,Weather_Timestamp,Temperature(F),Wind_Chill(F),Humidity(%),Pressure(in),Visibility(mi),Wind_Direction,Wind_Speed(mph),Precipitation(in),Weather_Condition,Amenity,Bump,Crossing,Give_Way,Junction,No_Exit,Railway,Roundabout,Station,Stop,Traffic_Calming,Traffic_Signal,Turning_Loop,Sunrise_Sunset,Civil_Twilight,Nautical_Twilight,Astronomical_Twilight, Duration, Weekday, Hour\n";
 
@@ -119,5 +123,15 @@ public class ManageCSV {
         return nextLine;
     }
 
+    public int getCountTuples() {
+        return countTuples;
+    }
 
+    public String getHeader() {
+        return header;
+    }
+
+    public int[] getCountSeverity() {
+        return countSeverity;
+    }
 }
